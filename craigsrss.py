b2ae6d02755bc56fce8d59b9944c4539641c58db
Jeremy Hurst <apophis981@gmail.com>
2019-05-07 18:18:59 -0700

    Now grabs name, id, region, and url from rss feed

--- craigsrss.py
+++ craigsrss.py
@@ -1,9 +1,23 @@
 import feedparser
-import json
+from pymongo import MongoClient
+import pprint
+import re
+
+client = MongoClient()
+
+db = client.test_database
+posts = db.posts
 
 d = feedparser.parse('https://sfbay.craigslist.org/search/apa?format=rss')
 items = (d['items'])
-urls  = set()
 for item in items:
-    urls.add(item['id'])
-print(urls)
+    url = item['id']
+    m = re.search('.*\/([a-z]+)\/apa\/d\/([^\/]+)\/(\d+)\.html', url)
+    region = m.group(1)
+    name = m.group(2)
+    id = m.group(3)
+    print(region)
+    print(name)
+    print(id)
+    #post_id = posts.insert_one(post).inserted_id
+#pprint.pprint(posts.find_one())
